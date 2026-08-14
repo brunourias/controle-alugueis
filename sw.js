05e09462eed7153b04dfc22325f36dcba039682c
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-14 18:23:52 -0300

    Renova cache da nova navegação mobile

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v109";
+const CACHE_NAME = "controle-alugueis-v110";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=109",
-  "./app.js?v=109",
+  "./styles.css?v=110",
+  "./app.js?v=110",
   "./manifest.json"
 ];
 
