30a1599801b8572e01cf809a26850b5194d75dcd
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-14 19:07:42 -0300

    Atualiza cache da revisão biométrica

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v120";
+const CACHE_NAME = "controle-alugueis-v121";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=120",
-  "./app.js?v=120",
+  "./styles.css?v=121",
+  "./app.js?v=121",
   "./manifest.json"
 ];
 
