9119db5eec4ee51c7a6fd38b8895843165965b3c
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-14 13:13:38 -0300

    Renova cache do escudo do cabeçalho

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v106";
+const CACHE_NAME = "controle-alugueis-v107";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=106",
-  "./app.js?v=106",
+  "./styles.css?v=107",
+  "./app.js?v=107",
   "./manifest.json"
 ];
 
