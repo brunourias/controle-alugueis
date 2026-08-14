83fbc4ad91f7914de01c782f8ac03e5819608dbe
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-14 19:03:27 -0300

    Atualiza cache do desbloqueio biométrico

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v119";
+const CACHE_NAME = "controle-alugueis-v120";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=119",
-  "./app.js?v=119",
+  "./styles.css?v=120",
+  "./app.js?v=120",
   "./manifest.json"
 ];
 
