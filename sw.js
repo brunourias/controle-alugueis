5e283b610eb0f08ddda3d8cda5d05d8b4fb5b34f
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-14 15:58:44 -0300

    Renova cache da administração de contas

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v107";
+const CACHE_NAME = "controle-alugueis-v108";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=107",
-  "./app.js?v=107",
+  "./styles.css?v=108",
+  "./app.js?v=108",
   "./manifest.json"
 ];
 
