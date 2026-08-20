f0c0dd334f442ecadc69a748a88272897b04b941
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-20 09:51:48 -0300

    Corrige precache da versão atual

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v133";
+const CACHE_NAME = "controle-alugueis-v134";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=129",
-  "./app.js?v=129",
+  "./styles.css?v=134",
+  "./app.js?v=134",
   "./manifest.json"
 ];
 
