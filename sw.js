62d4c844f2e98519b668415fb24a740922cc2a6c
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-20 14:14:22 -0300

    Atualiza cache da administração da plataforma

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v140";
+const CACHE_NAME = "controle-alugueis-v141";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=140",
-  "./app.js?v=140",
+  "./styles.css?v=141",
+  "./app.js?v=141",
   "./manifest.json"
 ];
 
