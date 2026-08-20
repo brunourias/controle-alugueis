409da96fcac30ea5e3325fca35bbd4cb06bef343
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-20 08:59:03 -0300

    Atualiza cache das tabelas do resumo anual

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v126";
+const CACHE_NAME = "controle-alugueis-v127";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=126",
-  "./app.js?v=126",
+  "./styles.css?v=127",
+  "./app.js?v=127",
   "./manifest.json"
 ];
 
