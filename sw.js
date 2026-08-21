f3fc1b2f03b457fe637876cab734f1d14fb42f8d
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-21 10:07:40 -0300

    Atualiza cache da consolidação financeira

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v145";
+const CACHE_NAME = "controle-alugueis-v146";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=145",
-  "./app.js?v=145",
+  "./styles.css?v=146",
+  "./app.js?v=146",
   "./manifest.json"
 ];
 
