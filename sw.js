ded7b52c48ba0530c6a91f8c350aab1d88ccf7fa
brunourias <64868814+brunourias@users.noreply.github.com>
2026-08-28 10:16:24 -0300

    Atualiza cache da formatacao monetaria

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "controle-alugueis-v204";
+const CACHE_NAME = "controle-alugueis-v205";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=204",
-  "./app.js?v=204",
+  "./styles.css?v=205",
+  "./app.js?v=205",
   "./manifest.json"
 ];
 
