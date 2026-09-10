601c932b6ef92de572c94b62e365d13dea433673
brunourias <64868814+brunourias@users.noreply.github.com>
2026-09-10 13:01:56 -0300

    Atualiza cache para v242

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "controle-alugueis-v241";
+const CACHE_NAME = "controle-alugueis-v242";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=241",
-  "./app.js?v=241",
-  "./energy-calculations.js?v=241",
+  "./styles.css?v=242",
+  "./app.js?v=242",
+  "./energy-calculations.js?v=242",
   "./manifest.json"
 ];
 
