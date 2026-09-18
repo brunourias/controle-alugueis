14b4914396753526b2b205ed5bb6766c9947476a
brunourias <64868814+brunourias@users.noreply.github.com>
2026-09-18 10:31:47 -0300

    Atualiza cache do aplicativo para versão 261

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "controle-alugueis-v260";
+const CACHE_NAME = "controle-alugueis-v261";
 
 const APP_SHELL = [
   "./index.html",
-  "./styles.css?v=260",
-  "./app.js?v=260",
-  "./energy-calculations.js?v=260",
+  "./styles.css?v=261",
+  "./app.js?v=261",
+  "./energy-calculations.js?v=261",
   "./manifest.json"
 ];
 
